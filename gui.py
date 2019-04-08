f8fea02f069343dda0682970d0aa27b3ddfd2190
nhaas1 <haas.1@themetroschool.org>
2019-04-08 16:22:35 -0400

    Added Submit button

--- gui.py
+++ gui.py
@@ -1,5 +1,41 @@
 import roboticshours
-import tkinter
+from tkinter import *
 
+root = Tk()
+root.title("Names")
 
-master = Tk()
+mainframe = Frame(root)
+mainframe.grid(column=2,row=2)
+mainframe.columnconfigure(2, weight = 1)
+mainframe.rowconfigure(2, weight = 1)
+mainframe.pack(pady = 200, padx = 100)
+
+tkvar = StringVar(root)
+
+#######################################
+choices = {'Nathan','Thomas','Evan','Josiah','Drew','Tyler'}
+tkvar.set('Tyler')
+
+popupMenu = OptionMenu(mainframe, tkvar, *choices)
+Label(mainframe, text="Choose your fighter").grid(row = 0, column = 1)
+popupMenu.grid(row = 1, column =1)
+
+def change_dropdown(*args):
+    print( tkvar.get() )
+
+tkvar.trace('w', change_dropdown)
+#######################################
+def callback():
+    print ("Submitted!")
+
+f = Frame(root, height=40, width=80)
+f.pack_propagate(0) # don't shrink
+f.pack()
+
+b = Button(f, text="SUBMIT", command=callback)
+b.grid(row = 2, column = 1)
+b.pack()
+
+#######################################
+
+root.mainloop()
